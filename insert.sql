e42b2eca4ed63674b0f0b409d5e5bac6304863ca
rasvindra <rasvindra@hotmail.com>
2022-07-27 16:04:44 -0700

    created schema and insert sql files and folder structure

---
+++ insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO person_position (id, first_name, last_name, title, salary)
+VALUES
+    ( 1, "Fred","Flinstone","Technician", 70000),
+    ( 2, "Wilma","Flinstone","Lead Technician",82000),
+    ( 3, "Bam Bam","Rubble","Product Tester",48000),
+    ( 4, "Pebbles","Flinstone","Design and Marketing", 68000),
+    ( 5, "Betty","Rubble","Sales Lead",90000),
+    ( 6, "Barney","Ruble","Gopher", 68000),
+    ( 7, "George","Jetson","Accountant",66666),
+    ( 8, "Jane","Jetson","Account Manager",80000);
+
+INSERT INTO incharge (id, department, manager)
+VALUES
+    ( 1, "Engineering","Conan the Barbarian"),
+    ( 2, "Finance","Scrooge McDuck"),
+    ( 3, "Quality Assurance","Norville Rogers"),
+    ( 4, "Facilities","Solomn Grundy");    
---
+++ schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_Tracker_db;
+
+USE employee_Tracker_db;
+
+CREATE TABLE person_position (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary INT NOT NULL
+);
+
+CREATE TABLE incharge (
+    id INT NOT NULL
+    department VARCHAR(30) NOT NULL,
+    manager VARCHAR(30),
+);
