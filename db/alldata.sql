2e6dc24b477e3811442479460eeac7e4d7728ab7
rasvindra <rasvindra@hotmail.com>
2022-07-28 19:57:44 -0700

    slight changes to queries

--- db/alldata.sql
+++ db/alldata.sql
@@ -3,18 +3,18 @@
 SELECT department.id AS "Department ID", department.deptname AS "Department Name" FROM department;
 
 
--- MANAGER ID NOT DISPLAYING CORRECTLY. ONLY SHOWS 1 or 2
+-- Query that displays position, its ID, salary the the department assosiated. WORKS!
 SELECT position.id , position.title, position.salary, department.deptname
 FROM position
    JOIN department
    ON position.deptid = department.id;
 
 
--- MANAGER ID NOT DISPLAYING CORRECTLY. ONLY SHOWS 1 or 2
+-- Query that displays employees and all associated data for employee. WORKS!
 SELECT position.id, position.title, position.salary, department.deptname, employee.first_name, employee.last_name, employee.manager_id
 FROM position 
 JOIN department 
 ON position.deptid = department.id
 JOIN employee
-ON employee.id = position.deptid
+ON position.id = employee.id
 
